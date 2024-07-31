199f03e6e13a17638b653030a505e61522261ba4
Antti Pirinen <e2202869@edu.vamk.fi>
2024-07-31 17:53:32 +0300

    Hello World!

--- boot.asm
+++ boot.asm
@@ -1,13 +1,29 @@
 ORG 0x7c00 ; starting address
 BITS 16
 
-_start:
-    mov ah, 0eh
-    mov al, 'X'
-    mov bh, 0
-    int 0x10 ; BIOS call
+message: db 'Hello World!', 0
 
+_start:
+    mov si, message
+    call print
     jmp $
+
+print:
+    mov bh, 0 ; page
+.loop:
+    lodsb ; loads chars from si to al
+    cmp al, 0
+    je .done
+    call print_char
+    jmp .loop
+.done:
+    ret
+
+print_char:
+    mov ah, 0eh
+    int 0x10 ; BIOS call
+    ret
+
 
 times 510-($-$$) db 0 ; fill the rest with 0's
 dw 0xAA55 ; boot signature
