1730bc710686e8de997c3439c0516c39e6a275cb
Antti Pirinen <e2202869@edu.vamk.fi>
2024-08-05 14:00:24 +0300

    protected mode, segment initialization

--- boot.asm
+++ boot.asm
@@ -1,11 +1,8 @@
 ORG 0x7c00
 BITS 16
 
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
-
 _start:
-    jmp short start
+    jmp short start ; jump over the disk format information
     nop
 
 times 33 db 0   ; making space for BPB https://wiki.osdev.org/FAT#BPB_(BIOS_Parameter_Block)
@@ -26,43 +23,58 @@ step2:
 .load_protected:
     cli
     lgdt[gdt_descriptor]
+
+    ; set PE (Protection Enable) bit in CR0 (Control Register 0)
     mov eax, cr0
     or eax, 0x1
     mov cr0, eax
-    jmp CODE_SEG:load32
+    jmp CODE_SEG:load32 ; far jump ( jmp to the other segment)
 
-; Global description table
+; Global description table 
+; https://wiki.osdev.org/Global_Descriptor_Table
+; https://www.youtube.com/watch?v=Wh5nPn2U_1w
+
 gdt_start:
-gdt_null:
+gdt_null:   ; empty descriptor
     dd 0x0
     dd 0x0
 
-; offset 0x8
-gdt_code:     ; CS should point to this
+gdt_code:
     dw 0xffff ; Segment limit first 0-15 bits
     dw 0      ; Base first 0-15 bits
     db 0      ; Base 16-23 bits
-    db 0x9a   ; Access byte
-    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 10011010   ; Access byte
+    db 11001111 ; High 4 bit flags and the low 4 bit flags
     db 0      ; Base 24-31 bits
 
-; offset 0x10
 gdt_data:     ; DS, SS, ES, FS, GS
     dw 0xffff ; Segment limit first 0-15 bits
     dw 0      ; Base first 0-15 bits
     db 0      ; Base 16-23 bits
-    db 0x92   ; Access byte
-    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 10010010   ; Access byte
+    db 11001111 ; High 4 bit flags and the low 4 bit flags
     db 0      ; Base 24-31 bits
 
 gdt_end: 
 
 gdt_descriptor:
-    dw gdt_end - gdt_start-1
-    dd gdt_start
+    dw gdt_end - gdt_start-1    ; size of the gdt
+    dd gdt_start                ; pointer to gdt
+
+; segment offsets
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 [BITS 32]
 load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
 
 times 510-($-$$) db 0 ; fill the rest with 0's
