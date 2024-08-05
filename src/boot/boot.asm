ad0eac6603950150679f500fe0fc56335639a745
Antti Pirinen <e2202869@edu.vamk.fi>
2024-08-05 14:20:37 +0300

    new folders and makefile

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -43,16 +43,16 @@ gdt_code:
     dw 0xffff ; Segment limit first 0-15 bits
     dw 0      ; Base first 0-15 bits
     db 0      ; Base 16-23 bits
-    db 10011010   ; Access byte
-    db 11001111 ; High 4 bit flags and the low 4 bit flags
+    db 10011010b   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
     db 0      ; Base 24-31 bits
 
 gdt_data:     ; DS, SS, ES, FS, GS
     dw 0xffff ; Segment limit first 0-15 bits
     dw 0      ; Base first 0-15 bits
     db 0      ; Base 16-23 bits
-    db 10010010   ; Access byte
-    db 11001111 ; High 4 bit flags and the low 4 bit flags
+    db 10010010b   ; Access byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
     db 0      ; Base 24-31 bits
 
 gdt_end: 
