8b41b8df90e7b0e720637bdc1061b1bfb04cc9c6
Antti Pirinen <e2202869@edu.vamk.fi>
2024-09-12 09:27:17 +0300

    New string functions

--- src/string/string.c
+++ src/string/string.c
@@ -1,5 +1,16 @@
 #include "string.h"
 
+/* returns character in lowercase */
+char tolower(char s1)
+{
+    if (s1 >= 65 && s1 <= 90)
+    {
+        s1 += 32;
+    }
+
+    return s1;
+}
+
 /* returns the string length */
 int strlen(const char *ptr)
 {
@@ -60,4 +71,71 @@ char *strcpy(char *dest, const char *src)
     *dest = '\0';
 
     return res;
+}
+
+/* compares n amount of characters of 2 strings
+ * returns 0 if characters are the same
+ * returns negative value if str1 is less than str2
+ * returns positive value if str1 is greater than str2
+ */
+int strncmp(const char *str1, const char *str2, int n)
+{
+    unsigned char u1, u2;
+
+    while (n-- > 0)
+    {
+        u1 = (unsigned char)*str1++;
+        u2 = (unsigned char)*str2++;
+
+        if (u1 != u2)
+        {
+            return u1 - u2;
+        }
+
+        if (u1 == '\0')
+        {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+/* strncmp function with no case sensitivity */
+int istrncmp(const char *s1, const char *s2, int n)
+{
+    unsigned char u1, u2;
+    while (n-- > 0)
+    {
+        u1 = (unsigned char)*s1++;
+        u2 = (unsigned char)*s2++;
+
+        if (u1 != u2 && tolower(u1) != tolower(u2))
+        {
+            return u1 - u2;
+        }
+
+        if (u1 == '\0')
+        {
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+/* returns the index of terminator */
+int strlen_terminator(const char *str, int max, char terminator)
+{
+    int i = 0;
+
+    for (i = 0; i < max; i++)
+    {
+        if (str[i] == '\0' || str[i] == terminator)
+        {
+            break;
+        }
+    }
+
+    return i;
 }
