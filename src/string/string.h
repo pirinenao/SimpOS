758cc82735af4c89b2d959d8fe2fdf16b322c022
Antti Pirinen <e2202869@edu.vamk.fi>
2024-09-12 09:29:46 +0300

    New string functions

--- src/string/string.h
+++ src/string/string.h
@@ -8,5 +8,9 @@ bool isdigit(char c);
 int tonumericdigit(char c);
 int strnlen(const char *ptr, int max_length);
 char *strcpy(char *dest, const char *src);
+char tolower(char s1);
+int strncmp(const char *str1, const char *str2, int n);
+int istrncmp(const char *s1, const char *s2, int n);
+int strlen_terminator(const char *str, int max, char terminator);
 
 #endif
